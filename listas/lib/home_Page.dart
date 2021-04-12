00ca26ec7a9a29daeb0688a8d09739ed6a951b72
Alexandre Abreu <contatoalexandre95@gmail.com>
2021-04-12 08:22:04 -0300

    Adicionando uma função card onde o usuário clica e o menu se expande com uma mensagem 'any text'

--- listas/lib/home_Page.dart
+++ listas/lib/home_Page.dart
@@ -1,28 +1,61 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage> {
+  final myList = List.generate(40, (index) => 'Index name: $index');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.lightGreen,
         title: Text("ListView"),
       ),
-      body: ListView.separated(
-        itemCount: 100,
-        itemBuilder: (_, index) {
-          return Text('Lista index $index');
-        },
-        separatorBuilder: (_, __) {
-          return Divider();
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.lightGreen,
+                    radius: 40,
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: ListView.builder(
+              itemCount: myList.length,
+              itemBuilder: (_, index) {
+                final listtileGenerated = myList
+                    .map((e) => Card(
+                          child: ExpansionTile(
+                            title: Text(myList[index]),
+                            leading: Icon(
+                              Icons.account_circle,
+                              size: 30,
+                            ),
+                            children: [
+                              Text('Any text...'),
+                            ],
+                          ),
+                        ))
+                    .toList();
+                return listtileGenerated[index];
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
